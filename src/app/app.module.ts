bc065f285222ec2140b5b4d17b9da6ee26f6b259
Javier Villarroel <javc2804@gmail.com>
2019-03-11 10:36:40 -0400

    Se agruparon los modulos, los cambios de temas se mantienen en un servicio, y las rutas del sidebar se manejan desde el controlador, usando objetos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,10 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
 import { FormsModule } from '@angular/forms';
 
+// Servicios
+import { ServiceModule } from './services/service.module';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +29,8 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     APP_ROUTES,
     PagesModule,
-    FormsModule
+    FormsModule,
+    ServiceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/platform-browser';
-import { SettingsService } from '../../services/settings.service';
+import { SettingsService } from '../../services/service.index';
 
 @Component({
   selector: 'app-account-settings',
@@ -12,6 +12,7 @@ export class AccountSettingsComponent implements OnInit {
   constructor( public _ajustes: SettingsService) { }
 
   ngOnInit() {
+    this.colocarCheck();
   }
 
   cambiarColor(thema: string, link: any) {
@@ -25,6 +26,17 @@ export class AccountSettingsComponent implements OnInit {
     for (const ref of selectores) {
       ref.classList.remove('working');
       link.classList.add('working');
+    }
+  }
+
+  colocarCheck() {
+    const selectores: any = document.getElementsByClassName('selector');
+    const tema = this._ajustes.ajustes.tema;
+    for (const ref of selectores) {
+      if ( ref.getAttribute('data-theme') === tema ) {
+        ref.classList.add('working');
+        break;
+      }
     }
   }
 
